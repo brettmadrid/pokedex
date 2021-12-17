38ce8daa23c92fd7751d58d01e836ca0380d84db
brettmadrid <brettmadrid9@gmail.com>
2021-12-16 20:40:18 -0800

    styled and referenced better pic library

--- src/Pokecard.js
+++ src/Pokecard.js
@@ -1,18 +1,28 @@
 import React, { Component } from 'react';
 import './Pokecard.css';
 const POKE_API =
-  'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
+  // 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
+  'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/';
 
 class Pokecard extends Component {
   render() {
     const { id, name, type, exp } = this.props;
-    let imgSrc = `${POKE_API}${id}.png`;
+    const addz = id => {
+      let newId = id.toString();
+      if (newId.length === 1) {
+        newId = `00${newId}`;
+      } else if (newId.length === 2) {
+        newId = `0${newId}`;
+      }
+      return newId;
+    };
+    let imgSrc = `${POKE_API}${addz(id)}.png`;
     return (
       <div className='Pokecard'>
-        <h1>{name}</h1>
+        <h1 className='Pokecard-title'>{name}</h1>
         <img src={imgSrc} alt={name} />
-        <div>Type: {type}</div>
-        <div>Experience: {exp}</div>
+        <div className='Pokecard-data'>Type: {type}</div>
+        <div className='Pokecard-data'>Experience: {exp}</div>
       </div>
     );
   }
